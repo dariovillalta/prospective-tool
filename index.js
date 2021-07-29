85b81ddd92cbc404741d596e7215d0140fa1b8d8
Dario Villalta <dariovillalta@Darios-MacBook-Pro.local>
2021-07-29 16:18:57 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send({ pregunta: 'Oye como va!' });
+});
+
+app.post('/', (req, res) => {
+    console.log("req", req);
+});
+
+app.listen(3333, () => {
+    console.log('Application listening on port 3333!');
+});
